483f607f27355309589d192fe0f23eac331b587f
Rubenizz <112633173+Ruben7307@users.noreply.github.com>
2023-09-11 13:06:11 +0200

    tried to fixx bug

--- frontend/src/components/pages/EventPage/Eventpage.tsx
+++ frontend/src/components/pages/EventPage/Eventpage.tsx
@@ -1,70 +1,64 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { eventData } from "../../../types/models/Event.model";
-import EventService from "../../../Services/EventService";
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import { eventData } from '../../../types/models/Event.model';
+import EventService from '../../../Services/EventService';
 
 export default function SimplePaper() {
   const navigate = useNavigate();
 
   const handleAdd = () => {
-    navigate("../addevent/");
+    navigate('../addevent/');
   };
 
-  const [events, setEvents] = React.useState<eventData[]>([]);
+  const [events, setEvents] = useState<eventData[]>([]);
 
-  React.useEffect(() => {
-    try {
-      EventService.getEvent().then((data) => {
-        setEvents(data);
-        console.log(data);
-      });
-
-      console.log(events);
-    } catch (error) {
-      console.error("Fehler beim Abrufen der Events: ", error);
-    }
+  useEffect(() => {
+    // Load events when the component mounts
+    loadEvents();
   }, []);
 
-  const submitActionHandler = (values: eventData) => {
-    EventService.createEvent(values).then(() => {
-      navigate("/users");
-    });
+  const loadEvents = async () => {
+    try {
+      const data = await EventService.getEvent();
+      setEvents(data);
+    } catch (error) {
+      console.error('Fehler beim Abrufen der Events: ', error);
+    }
+  };
+
+  const deleteEvent = async (id: string | number) => {
+    try {
+      await EventService.deleteEventById(id);
+      // Reload events after deleting
+      loadEvents();
+      console.log('Event successfully deleted');
+    } catch (error) {
+      console.error('Fehler beim Löschen des Events: ', error);
+    }
   };
 
   return (
     <div>
       <h1>Events</h1>
-      
-      <Button
-        size="small"
-        color="success"
-        variant="contained"
-        onClick={handleAdd}
-      >
-        Add
 
-      </Button>{" "}
+      <Button size="small" color="success" variant="contained" onClick={handleAdd}>
+        Add
+      </Button>{' '}
       <Box>
         <Grid container spacing={1}>
           {events.map((event, index) => (
-            <Grid
-              item
-              key={index}
-              xs={2}
-              marginTop={"10px"}
-              marginLeft={"45px"}
-            >
+            <Grid item key={index} xs={2} marginTop="10px" marginLeft="45px">
               <Paper elevation={5} sx={{ padding: 2 }}>
                 <h2>{event.eventName}</h2>
                 <div>Location: {event.location}</div>
                 <div>Date: {event.date}</div>
                 <br />
-                <button>Edit Event</button>
-                <button>Delete Event</button>
+                <button onClick={() => navigate(`/editevent/${event.id}`)}>Edit Event</button>
+                <button onClick={() => deleteEvent(event.id)}>Delete Event</button>
               </Paper>
             </Grid>
           ))}
