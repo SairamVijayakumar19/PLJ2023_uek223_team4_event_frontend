27502a611b0461155e2bbb4e012cafb77e8b9bf5
Ruben7307 <ruben.schneebeli@noseryoung.com>
2023-09-11 16:08:40 +0200

    test

--- frontend/src/types/models/Event.model.ts
+++ frontend/src/types/models/Event.model.ts
@@ -1,10 +1,14 @@
+import { User } from "./User.model";
 
-
+// test
 export interface eventData {
     id: string;
-    guestList: string;
+    guestList: User[];
     eventName: string;
     date: string;
     location: string;
   }
+
+  export type createEventData = Omit<eventData,"id">
+
   
